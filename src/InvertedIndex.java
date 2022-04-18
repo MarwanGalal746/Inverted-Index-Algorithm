e3099ae8149c28f884e29d73a8dd096099125898
marwangalal746 <marawangalal84@gmail.com>
2022-04-18 15:05:05 +0200

    logic operators

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -3,21 +3,24 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class InvertedIndex {
-    private HashMap<String, ArrayList<String>> postingsArray;
+    private HashMap<String, ArrayList<String>> postingsList;
 
     public InvertedIndex() {
-        this.postingsArray = new HashMap<>();
+        this.postingsList = new HashMap<>();
+    }
+
+    public void printPostingsList() {
+        for (Map.Entry<String, ArrayList<String>> entry : this.postingsList.entrySet()) {
+            System.out.print(entry.getKey() + "/ ");
+            for (int i = 0; i < entry.getValue().size(); i++)
+                System.out.print(entry.getValue().get(i));
+            System.out.println();
+        }
+
     }
 
     public void AddDocs(ArrayList<String> docs) {
         tokenizingAndSorting(docs.get(0));
-
-        for (Map.Entry<String, ArrayList<String>> entry : this.postingsArray.entrySet()) {
-            System.out.print(entry.getKey() + "/ " );
-            for (int i=0 ; i< entry.getValue().size() ; i++)
-                System.out.print(entry.getValue().get(i));
-            System.out.println();
-        }
 
     }
 
@@ -28,13 +31,13 @@ public class InvertedIndex {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 StringTokenizer tokenizer = new StringTokenizer(data);
-                while(tokenizer.hasMoreTokens()) {
+                while (tokenizer.hasMoreTokens()) {
                     String word = tokenizer.nextToken();
-                    if (this.postingsArray.get(word) == null)
-                        this.postingsArray.put(word, new ArrayList<String>(Arrays.asList(file)));
+                    if (this.postingsList.get(word) == null)
+                        this.postingsList.put(word, new ArrayList<String>(Arrays.asList(file)));
                     else {
-                        if (!this.postingsArray.get(word).contains(file)) {
-                            this.postingsArray.get(word).add(file);
+                        if (!this.postingsList.get(word).contains(file)) {
+                            this.postingsList.get(word).add(file);
                         }
                     }
                 }
@@ -45,4 +48,52 @@ public class InvertedIndex {
             e.printStackTrace();
         }
     }
+
+    private ArrayList<String> and(ArrayList<String> list1, ArrayList<String> list2) {
+        ArrayList<String> result = new ArrayList<String>();
+        int i = 0, r = 0;
+        for (; i < list1.size() && r < list2.size(); ) {
+            if (list1.get(i) == list2.get(r)) {
+                result.add(list2.get(r));
+                i++;
+                r++;
+            } else if (list1.get(i).compareTo(list2.get(r)) < 0)
+                i++;
+            else
+                r++;
+        }
+        return result;
+    }
+
+    private ArrayList<String> or(ArrayList<String> list1, ArrayList<String> list2) {
+        ArrayList<String> result = new ArrayList<String>();
+        for (int i = 0; i < list1.size(); i++) {
+            result.add(list1.get(i));
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            if(!result.contains(list2.get(i))) {
+                result.add(list2.get(i));
+            }
+        }
+        return result;
+    }
+
+    private ArrayList<String> not(ArrayList<String> list1, ArrayList<String> list2) {
+        ArrayList<String> result = new ArrayList<String>();
+        int i = 0, r = 0;
+        for (; i < list1.size() && r < list2.size(); ) {
+            if (list1.get(i) == list2.get(r)) {
+                i++;
+                r++;
+            } else if (list1.get(i).compareTo(list2.get(r)) < 0){
+                result.add(list1.get(i));
+                i++;
+            }
+            else
+                r++;
+        }
+        return result;
+    }
+
+
 }
